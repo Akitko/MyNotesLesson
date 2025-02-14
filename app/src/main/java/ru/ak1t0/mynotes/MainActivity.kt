fcd840104a303f5f3bd48d44a4dca8182f26a029
Akitko <mrHarker33@gmail.com>
2025-02-14 18:44:01 +0300

    Lesson 1

--- app/src/main/java/ru/ak1t0/mynotes/MainActivity.kt
+++ app/src/main/java/ru/ak1t0/mynotes/MainActivity.kt
@@ -1,47 +1,62 @@
 package ru.ak1t0.mynotes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.ak1t0.mynotes.navigation.NotesNavHost
 import ru.ak1t0.mynotes.ui.theme.MyNotesTheme
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyNotesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Notes App")
+                            },
+                            backgroundColor = Color.DarkGray,
+                            contentColor = Color.LightGray,
+                            elevation = 12.dp
+                        )
+                    },
+                    content = {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.background
+                        ){
+                            NotesNavHost()
+                        }
+
+                    }
+                )
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyNotesTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/ru/ak1t0/mynotes/screens/Start.kt
@@ -0,0 +1,10 @@
+package ru.ak1t0.mynotes.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text="Notes App Start Screen")
+}
